1a7f9dea680bfe8ceb06d4d1bc08baa35f31613f
pavalex <Alex.P79@mail.ru>
2022-04-10 11:35:03 +0800

    сделал задание 11

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 "use strict";
 
+const button = document.querySelector('#btn');
+const buttonInSquare = document.querySelector('#e_btn');
+const text = document.querySelector('#text');
+const square = document.querySelector('#square');
+const range = document.querySelector('#range');
+const rangeValue = document.querySelector('#range-span');
+const circle = document.querySelector('#circle');
+
+button.addEventListener('click', function () {
+    square.style.backgroundColor = text.value;
+    text.value = '';
+});
+
+buttonInSquare.addEventListener('click', function () {
+    buttonInSquare.style.display = 'none';
+});
+
+range.addEventListener('input', function () {
+    rangeValue.textContent = range.value;
+    circle.style.width = range.value + '%';
+    circle.style.height = range.value + '%';
+});
+
+
 
 
 
